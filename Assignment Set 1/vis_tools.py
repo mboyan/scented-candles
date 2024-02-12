9ded5350aebe6d4b46d1ef02097fbd290825ca49
Boyan Mihaylov <mih.boyan@gmail.com>
2024-02-12 11:27:34 +0100

    Diffusion Equation formulation

--- Assignment Set 1/vis_tools.py
+++ Assignment Set 1/vis_tools.py
@@ -3,29 +3,49 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 from IPython.display import HTML
 
-def animate_frames(frames):
+def animate_strings(frames, interval=10):
     """
-    Creates a 2D animation of a vibrating string from the provided frames.
+    Creates a 2D animation of a series of vibrating strings from the provided frames.
     input:
-        frames (numpy.ndarray) - a 2D array containing the frames for the animation
+        frames (numpy.ndarray) - a 3D array containing the frames for the animation of each string
     output:
         an HTML animation
     """
 
-    assert frames.ndim == 2, 'frames must be a 3D array'
+    assert frames.ndim == 3, 'frames must be a 3D array'
 
-    n_frames = frames.shape[0]
+    n_frames = frames.shape[1]
 
-    # fig = plt.figure()
-    # plot,  = plt.plot(frames[0])
-    ax = plt.subplot()
-    fig = ax.get_figure()
-    plot, = ax.plot(frames[0])
+    fig, axs = plt.subplots(1, frames.shape[0], sharex=True, sharey=True)
+    fig.set_size_inches(8, 7/frames.shape[0])
+    plots = [axs[i].plot(np.linspace(0, 1, frames.shape[2]), frames[i][0])[0] for i in range(frames.shape[0])]
+    # plot, = ax.plot(np.linspace(0, 1, frames.shape[1]), frames[0])
+    time_txt = [ax.text(0.05, 0.05, '', transform=ax.transAxes) for ax in axs]
+
+    def init_anim():
+        """
+        Initialize animation
+        """
+        for j, plot in enumerate(plots):
+            plot.set_ydata(frames[j][0])
+            time_txt[j].set_text('')
+            axs[j].set_xlabel('$x$')
+            axs[j].set_ylabel('$u(x,t)$')
+            axs[j].set_title(f'Init. condition {j}')
+        return *plots, *time_txt
 
     def update(i):
-        plot.set_data(np.linspace(0, 1, frames[i].shape[0]), frames[i])
-        return plot,
+        """
+        Update animation
+        """
+        for j, plot in enumerate(plots):
+            plot.set_ydata(frames[j][i])
+            time_txt[j].set_text(f'$t={i}$')
+        return *plots, *time_txt
 
-    anim = animation.FuncAnimation(fig, update, frames=n_frames, interval=10, blit=True)
+    anim = animation.FuncAnimation(fig, update, init_func=init_anim, frames=n_frames, interval=interval, blit=True)
+
+    plt.tight_layout()
+    plt.show()
 
     return HTML(anim.to_html5_video())
