020e6e1cd9e7063acaad7329486a3b6d5bb3a3ed
Boyan Mihaylov <mih.boyan@gmail.com>
2024-02-28 14:33:16 +0100

    Eta parameter plots

--- Assignment Set 2/vis_tools.py
+++ Assignment Set 2/vis_tools.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 def plot_dla_diffusion(c_grid, cluster_grid, ax=None, title=None):
     """
@@ -27,4 +28,32 @@ def plot_dla_diffusion(c_grid, cluster_grid, ax=None, title=None):
 
     ax.set_xlabel('x')
     ax.set_ylabel('y')
+    plt.show()
+
+
+def plot_dla_diff_sim_params(df_sim_results):
+    """
+    Creates two plots showing the effect of the eta parameter
+    over the optimal omega for the SOR method and
+    the fractal dimension of the DLA cluster.
+    arguments:
+        df_sim_results (pandas.DataFrame): The simulation results.
+    """
+
+    fig, ax = plt.subplots(2, 1, figsize=(4, 7), sharex=True)
+
+    sns.scatterplot(data=df_sim_results, x='$\eta$', y='$\omega$', ax=ax[0])
+
+    sns.lineplot(data=df_sim_results, x='$\eta$', y='$\omega$', ax=ax[0])
+    ax[0].set_title('Optimal Omega for SOR Method')
+    ax[0].grid()
+
+    sns.scatterplot(data=df_sim_results, x='$\eta$', y='$D_r$', ax=ax[1])
+
+    sns.lineplot(data=df_sim_results, x='$\eta$', y='$D_r$', ax=ax[1])
+    ax[1].set_title('Fractal Dimension of DLA Cluster')
+    ax[1].grid()
+
+    plt.tight_layout()
+
     plt.show()
